7b2b141e465c26996ac5efc89fca52ad5d10d190
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-05-10 11:19:13 +0600

    Set routs and create components and also navber created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Navebar from './pages/Shared/Navebar';
+import Appointment from './pages/Appointment/Appointment';
+import Reviews from './Reviews/Reviews';
+import Contact from './Contact/Contact';
+import Login from './Register/Login';
 
 function App() {
   return (
     <div className="">
-      
+      <Navebar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/appointment" element={<Appointment />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ module.exports = {
   plugins: [require("daisyui")],
 
   daisyui:{
-    darkTheme: "light",
+    themes: [
+      {
+        doctortheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "light",
+      "cupcake",
+    ],
   },
 }
