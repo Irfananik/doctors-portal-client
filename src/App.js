6f085ba2ff3872dc747b0e474742b8d1486ca9e9
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-05-11 21:34:11 +0600

    footer section complete

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Appointment from './pages/Appointment/Appointment';
 import Reviews from './Reviews/Reviews';
 import Contact from './Contact/Contact';
 import Login from './Register/Login';
+import Footer from './pages/Shared/Footer';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
       </Routes>
+      <Footer/>
     </div>
   );
 }
---
+++ src/pages/Shared/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer style={{ background: `url(${'https://i.ibb.co/7zfK8Bt/footer.png'})`, backgroundSize:'cover' }} className="p-10 lg:mt-20 mt-12">
+            <div className='footer'>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </div>
+            </div>
+            <div className='my-10 text-center'>
+                <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
